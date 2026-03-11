c9f45dcec6312bd43af624a8bcd3c17271fac833
Ericgi231 <ericgi231@gmail.com>
2026-03-10 22:15:28 -0500

    Fixed error with new google analytics

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/Theme.ts';
@@ -8,9 +9,11 @@ import { GlobalStyles } from './styles/GlobalStyles.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
